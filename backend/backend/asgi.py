55e80039faf5e0e246ad790ffdfbbb30d24d6246
hikari <xxhyxayxx0619@gmail.com>
2025-01-18 14:22:20 +0900

    update files

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -2,14 +2,13 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from accounts.routing import websocket_urlpatterns  # 静的インポート
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            __import__('accounts.routing').routing.websocket_urlpatterns  # 動的にインポート
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
